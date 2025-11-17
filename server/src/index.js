e4766a1f7b053e29bd3fe02014ee785b36487426
splash0047 <your-email@example.com>
2025-11-18 00:05:25 +0530

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  });
+  
